ed971076952ea8d465576c4668e544fd1604c715
Xavier Bouquiaux <xavier.bouquiaux@acsone.eu>
2022-07-15 15:58:43 +0200

    [ADD] account_wallet

---
+++ account_wallet/models/__init__.py
@@ -0,0 +1,7 @@
+from . import account_wallet_type
+from . import account_wallet
+from . import account_move_line
+from . import account_move
+from . import account_chart_template
+from . import account_payment
+from . import wallet_config
