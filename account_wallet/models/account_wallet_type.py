9582751d0e9db6de1706527209665fd322703810
Sylvain LE GAL <sylvain.legal@grap.coop>
2022-07-25 15:58:18 +0200

    [REF] account_wallet : remove useless comment

--- account_wallet/models/account_wallet_type.py
+++ account_wallet/models/account_wallet_type.py
@@ -51,40 +51,6 @@ class AccountWalletType(models.Model):
         help="Check this box if you want to generate only nominative wallets."
     )
 
-    # TODO: Check if this is necessary and if model cannot be simplified
-    # @api.constrains('product_id', 'account_id', 'journal_id')
-    # def _check_account(self):
-    #     """ Check account defined on product is the same than account defined
-    #         on wallet
-    #         Check account defined on journal is the same than account defined
-    #         on wallet
-    #     """
-    #     check_ok = True
-    #     for wallet in self:
-    #         if wallet.product_id:
-    #             product_account_id = wallet.product_id.\
-    #                 property_account_income_id.id
-    #             if not product_account_id:
-    #                 product_account_id = wallet.product_id.categ_id.\
-    #                     property_account_income_categ_id.id
-    #             if not product_account_id or \
-    #                     product_account_id != wallet.account_id.id:
-    #                 check_ok = False
-    #                 break
-    #         journal_loss_account_id = wallet.journal_id.\
-    #             loss_account_id.id
-    #         if journal_loss_account_id != wallet.account_id.id:
-    #             check_ok = False
-    #             break
-    #         journal_profit_account_id = wallet.journal_id.\
-    #             profit_account_id.id
-    #         if journal_profit_account_id != wallet.account_id.id:
-    #             check_ok = False
-    #             break
-    #     if not check_ok:
-    #         raise ValidationError(_('Accounts not corresponding between '
-    #                                 'product, journal and cagnotte'))
-
     _sql_constraints = [
         (
             "product_wallet_type_uniq",
